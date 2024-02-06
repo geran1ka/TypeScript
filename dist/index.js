b6bee0986f247b7fb535c70509655bd199d56120
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-02-06 23:02:35 +0300

    35:40

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,60 @@
 "use strict";
-const div = (a, b, rounding) => {
-    const res = a / b;
-    if (rounding) {
-        return Math.round(res);
+var StatusStudent;
+(function (StatusStudent) {
+    StatusStudent["enrollee"] = "enrollee";
+    StatusStudent["student"] = "student";
+    StatusStudent["graduete"] = "graduete";
+    StatusStudent["bachelor"] = "bachelor";
+})(StatusStudent || (StatusStudent = {}));
+class Student {
+    constructor(name, courseOrAge, age) {
+        this.id = Math.random().toString(32).substring(2, 6) +
+            Date.now().toString().substring(9);
+        this.status = StatusStudent.enrollee;
+        this.createAt = new Date();
+        this._module = 0;
+        this.name = name;
+        if (typeof courseOrAge === "string") {
+            this.course = courseOrAge;
+            this.changeStatus(StatusStudent.student);
+        }
+        if (typeof courseOrAge === "number") {
+            this.age = courseOrAge;
+        }
+        if (age) {
+            this.age = age;
+        }
     }
-    return res;
-};
-div(15, 4, true);
-div(15, 3, false);
-div(10, 2);
-const fetcData = (res) => {
-    if (res.status === "pending") {
-        res.status;
+    set module(module) {
+        this._module = module;
+        this.updateAt = new Date();
     }
-    if (res.status === "success") {
-        res.data;
+    changeStatus(status) {
+        this.status = status;
+        this.updateAt = new Date();
     }
-    if (res.status === "failed") {
-        res.error;
+    changeInfo(courseOrModule, module) {
+        if (typeof courseOrModule === "string") {
+            this.course = courseOrModule;
+        }
+        if (typeof courseOrModule === "number") {
+            this.module = courseOrModule;
+        }
+        if (module) {
+            this.module = module;
+        }
+        this.updateAt = new Date();
     }
-};
+}
+const student1 = new Student("Петр");
+student1.changeInfo(3);
+console.log("student1: ", student1);
+const student2 = new Student("Дмитрий", "Frontend");
+student2.changeInfo("CSS");
+console.log("student2: ", student2);
+const student3 = new Student("Артур", 18);
+student3.changeInfo(2);
+console.log("student3: ", student3);
+const student4 = new Student("Геннадий", "JS", 18);
+student4.changeInfo("TS", 3);
+console.log("student4: ", student4);
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,88 @@
-const div = (a: number, b: number, rounding?: boolean) => {
-  const res = a / b;
+enum StatusStudent {
+  enrollee = "enrollee",
+  student = "student",
+  graduete = "graduete",
+  bachelor = "bachelor",
+}
 
-  if (rounding) {
-    return Math.round(res);
+class Student {
+  id: string =
+    Math.random().toString(32).substring(2, 6) +
+    Date.now().toString().substring(9);
+  status: StatusStudent = StatusStudent.enrollee;
+  createAt: Date = new Date();
+  updateAt?: Date;
+  name: string;
+  course?: string;
+  age?: number;
+  _module: number = 0;
+
+  // перегрузка конструктора
+  constructor(name: string);
+  constructor(name: string, course: string);
+  constructor(name: string, age: number);
+  constructor(name: string, course: string, age: number);
+  constructor(name: string, courseOrAge?: string | number, age?: number) {
+    this.name = name;
+    if (typeof courseOrAge === "string") {
+      this.course = courseOrAge;
+      this.changeStatus(StatusStudent.student);
+    }
+
+    if (typeof courseOrAge === "number") {
+      this.age = courseOrAge;
+    }
+
+    if (age) {
+      this.age = age;
+    }
   }
 
-  return res;
-};
-
-div(15, 4, true);
-
-div(15, 3, false);
-div(10, 2);
-
-type Student = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  bornCity?: string;
-};
-
-type HttpResponsePending = {
-  status: "pending";
-};
-
-type HttpResponseSuccess = {
-  status: "success";
-  data?: [];
-};
-
-type HttpResponseFailed = {
-  status: "failed";
-
-  error?: string;
-};
-
-type HttpResponse =
-  | HttpResponsePending
-  | HttpResponseSuccess
-  | HttpResponseFailed;
-
-const fetcData = (res: HttpResponse): void => {
-  if (res.status === "pending") {
-    res.status;
+  set module(module: number) {
+    this._module = module;
+    this.updateAt = new Date();
   }
 
-  if (res.status === "success") {
-    res.data;
+  changeStatus(status: StatusStudent): void {
+    this.status = status;
+    this.updateAt = new Date();
   }
 
-  if (res.status === "failed") {
-    res.error;
+  // перегрузка метода
+  changeInfo(course: string): void;
+  changeInfo(module: number): void;
+  changeInfo(course: string, module: number): void;
+  changeInfo(courseOrModule: string | number, module?: number): void {
+    if (typeof courseOrModule === "string") {
+      this.course = courseOrModule;
+    }
+
+    if (typeof courseOrModule === "number") {
+      this.module = courseOrModule;
+    }
+
+    if (module) {
+      this.module = module;
+    }
+    this.updateAt = new Date();
   }
-};
+}
+
+const student1: Student = new Student("Петр");
+student1.changeInfo(3);
+
+console.log("student1: ", student1);
+
+const student2: Student = new Student("Дмитрий", "Frontend");
+student2.changeInfo("CSS");
+
+console.log("student2: ", student2);
+
+const student3: Student = new Student("Артур", 18);
+student3.changeInfo(2);
+
+console.log("student3: ", student3);
+
+const student4: Student = new Student("Геннадий", "JS", 18);
+student4.changeInfo("TS", 3);
+console.log("student4: ", student4);
